cc0d2e986280fcfbee6617ac984b7e7d11fb8760
shreya <shreya@gmail.com>
2026-04-24 17:45:51 +0530

    changes

--- Odcet testing/addition-ui/src/App.jsx
+++ Odcet testing/addition-ui/src/App.jsx
@@ -32,8 +32,8 @@ function App() {
       <div className="card">
         <div className="card-header">
           <span className="icon">➕</span>
-          <h1>karumanchi</h1>
-          <p className="subtitle">Enter two numbers to add</p>
+          <h1>sravani</h1>
+          <p className="subtitle">Enter three numbers to add</p>
         </div>
 
         {/* Visual equation display */}
