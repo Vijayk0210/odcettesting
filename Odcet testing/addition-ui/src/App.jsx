5549f56e5fee0ba1c2bcda67284851ac449c231b
Gujjala Sravani <sravanirmn@gmail.com>
2026-04-26 18:40:00 +0530

    Changes made

--- Odcet testing/addition-ui/src/App.jsx
+++ Odcet testing/addition-ui/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 
           <h1>G</h1>
 
-          <p className="subtitle">Enter two numbers for adding</p>
+          <p className="subtitle">Enter two numbers to add</p>
 
           <h1>sravani</h1>
           <p className="subtitle">Enter three numbers to add</p>
