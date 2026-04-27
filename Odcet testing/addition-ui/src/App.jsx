ee110221226bcf62d8dcc8c9b362bd798f3cfaab
durgaprasad297 <durgaprasadmenda30@gmail.com>
2026-04-27 15:16:19 +0530

    Initial Commit

--- Odcet testing/addition-ui/src/App.jsx
+++ Odcet testing/addition-ui/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
       <div className="card">
         <div className="card-header">
           <span className="icon">➕</span>
-          <h1>Addition Calculator</h1>
+          <h1>prasad</h1>
           <p className="subtitle">Enter two numbers to add them together</p>
         </div>
 
